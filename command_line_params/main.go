3382883771bed554d44b6e811d2b9be3c5e382a4
krls08 <krls.pjds08@gmail.com>
2022-06-05 16:38:55 +0200

    add files

---
+++ command_line_params/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("h")
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/krls08/examples-go
+
+go 1.18
